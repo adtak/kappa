42f196df888891cba6977d1b2e67d8f0f3934f17
adtak <Takeyuki@adtak-Mac.local>
2020-08-12 23:48:20 +0900

    feat: add sqlalchemy model

--- src/db/models.py
+++ src/db/models.py
@@ -5,15 +5,29 @@ from sqlalchemy.schema import Column
 from sqlalchemy.sql.schema import ForeignKey
 from sqlalchemy.types import DateTime, Float, Integer, String
 
+from src.db.connection import DataBaseConnection
 
 Base = declarative_base()
+
+
+class Apartment(Base):
+    __tablename__ = "apartments"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    address = Column(String, nullable=False)
+    station = Column(String)
+    walk_time = Column(Integer)
+    rooms = relationship("Room", uselist=True)
+    created_at = Column(DateTime, nullable=False, server_default=func.now())
+    updated_at = Column(DateTime, nullable=False, server_default=func.now(), server_onupdate=func.now())  # noqa E501
 
 
 class Room(Base):
     __tablename__ = "rooms"
 
     id = Column(Integer, primary_key=True)
-    apartment_id = Column(Integer, ForeignKey("apartments.id", nullable=False))
+    apartment_id = Column(Integer, ForeignKey("apartments.id"), nullable=False)
     room_number = Column(String, nullable=False)
     layout = Column(String)
     size = Column(String)
@@ -21,3 +35,11 @@ class Room(Base):
     apartment = relationship("Apartment")
     created_at = Column(DateTime, nullable=False, server_default=func.now())
     updated_at = Column(DateTime, nullable=False, server_default=func.now(), server_onupdate=func.now())  # noqa E501
+
+
+def main():
+    Base.metadata.create_all(DataBaseConnection().engine)
+
+
+if __name__ == "__main__":
+    main()
