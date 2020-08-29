681bde2711af04fba4d75d6c6df943f153f9096a
adtak <Takeyuki@adtak-Mac.local>
2020-08-29 22:27:06 +0900

    chore: load database url from env to alembic

--- src/db/migrations/env.py
+++ src/db/migrations/env.py
@@ -1,3 +1,5 @@
+import os
+
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
@@ -11,6 +13,8 @@ import src.db.models as my_models
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+# overwrites the database url
+config.set_main_option('sqlalchemy.url', os.environ["DATABASE_URL"])
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
